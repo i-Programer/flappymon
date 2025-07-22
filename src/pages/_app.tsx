f0ecd45a2d0bd9f02349dbbec35d945c4008708c
i-Programer <ashof.z17@gmail.com>
2025-07-23 02:50:57 +0700

    fix the metamask cannot connect error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { WagmiProvider } from 'wagmi'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiConfig, WagmiProvider } from "wagmi";
 import { config } from "@/lib/wagmi";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}> 
-        <Component {...pageProps} />
-      </QueryClientProvider> 
-    </WagmiProvider>
-  ) 
+    return (
+        <WagmiConfig config={config}>
+            <QueryClientProvider client={queryClient}>
+                <Component {...pageProps} />
+            </QueryClientProvider>
+        </WagmiConfig>
+    );
 }
+
